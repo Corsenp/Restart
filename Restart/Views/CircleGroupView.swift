438d4e460da99cde7281fdaab05a9f69dd3e7e46
Corsenac Pierre <pierre.corsenac@epitech.eu>
2022-06-14 15:48:47 +0200

    [COMPONENT] [CIRCLEGROUP] : creating a reusable circle component

---
+++ Restart/Views/CircleGroupView.swift
@@ -0,0 +1,20 @@
+//
+//  CircleGroupView.swift
+//  Restart
+//
+//  Created by Pierre Corsenac on 14/06/2022.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleGroupView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleGroupView()
+    }
+}

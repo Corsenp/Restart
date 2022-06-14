61c043fd6cc0533debfeea323082ce593c12f111
Corsenac Pierre <pierre.corsenac@epitech.eu>
2022-06-14 09:02:21 +0200

    [INIT] : Setting up Xcode project

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Pierre Corsenac on 14/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
